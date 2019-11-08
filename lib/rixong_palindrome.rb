d9eb28b26c03dd931a6a00160af8df8ced544416
EC2 Default User <ec2-user@ip-172-31-14-65.us-east-2.compute.internal>
2019-11-08 13:27:08 +0000

    Refactored to module. Added integer support.

--- lib/rixong_palindrome.rb
+++ lib/rixong_palindrome.rb
@@ -1,16 +1,28 @@
 require "rixong_palindrome/version"
 
-class String
+module RixongPalindrome
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
+    if processed_content.empty?
+      false
+    else
     processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z \d]/i).join.downcase
     end
+end
+
+class String
+  include RixongPalindrome
+end
+
+class Integer
+  include RixongPalindrome
 end
